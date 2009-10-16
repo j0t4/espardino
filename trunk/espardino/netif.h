3b7bba66fd7909f8559619b7a86da6cd6798d226
admin@nbee.es <admin@nbee.es@3a741920-7905-11de-8223-e15ac962e7eb>
2009-10-16 18:10:37 +0000

    common network interface

---
+++ trunk/espardino/netif.h
@@ -0,0 +1,50 @@
+/***************************************************************/
+/* Copyright (c) 2009 NBEE Embedded Systems S.L.               */
+/* All rights reserved.                                        */
+/*                                                             */
+/* Permission is hereby granted, free of charge, to any        */
+/* person obtaining a copy of this software and associated     */
+/* documentation files (the "Software"), to deal in the        */
+/* Software without restriction, including without limitation  */
+/* the rights to use, copy, modify, merge, publish,            */
+/* distribute, sublicense, and/or sell copies of the           */
+/* Software, and to permit persons to whom the Software is     */
+/* furnished to do so, subject to the following conditions:    */
+/*                                                             */
+/* This code is distributed only with and for Espardino(tm)    */
+/* products or derivative final products not intended as       */
+/* development systems.                                        */
+/*                                                             */
+/* The above copyright notice and this permission notice       */
+/* shall be included in all copies or substantial portions of  */
+/* the Software.                                               */
+/*                                                             */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY   */
+/* KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE  */
+/* WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR     */
+/* PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS  */
+/* OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR    */
+/* OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR  */
+/* OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE   */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.      */
+/***************************************************************/
+#ifndef NETIF_H_
+#define NETIF_H_
+
+typedef  struct {
+
+	unsigned char *(*get_mac_address)(void);
+	void (*set_mac_address)(unsigned char *mac_addr);
+	int (*is_linked)(void);
+	void (* relink)();
+	void (* reset)();
+	int (* wait_data)(int timeout);
+	void (* process)(void);
+	int (* tx_data)(unsigned char *data, int len);
+	int (* rx_data)(unsigned char *data, int maxlen);
+
+
+} st_net_driver_ops;
+
+
+#endif /* NETIF_H_ */
