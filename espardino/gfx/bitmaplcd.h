19ef7d69e0f8c06c5fef957f27974d82ea7ffaa0
admin@nbee.es <admin@nbee.es@3a741920-7905-11de-8223-e15ac962e7eb>
2010-11-22 13:06:40 +0000

    font and printf support for the oled driver

--- espardino/gfx/bitmaplcd.h
+++ espardino/gfx/bitmaplcd.h
@@ -3,23 +3,41 @@
 #include <micro214x.h>
 #include <hasprintf.h>
 
+#define FONT_8x16 1
+#define FONT_6x8  2
+
+#define FONT_INVERTED true
+#define FONT_NORMAL   false
+
+#define TRANSPARENT  1
+#define INVERTED	 2
+
 class BitmapLCD : public HasPrintf
 {
 	private:
 		int m_x_size;
 		int m_y_size;
 		int m_x,m_y;
+		bool m_inv;
 		unsigned char *scr;
-		
+
+		int f_width,f_height;
+		const unsigned char *f_data;
+
 	public:
+		BitmapLCD();
 		void setSize(int x,int y) { m_x_size=x; m_y_size=y; }
-		void line(int x0,int y0,int x1,int y1,int c);
-	   int write(char data); /* for printf compatibility */
-	  void gotoxy(int x,int y);
-	  void putImg(unsigned char *img,int x, int y, int opts);
-	  virtual plot(int x, int y, int c)=0;
-	  virtual clear()=0;
-};	
+	 	void line(int x0,int y0,int x1,int y1,int c);
+
+	 	void gotoxy(int x,int y);
+	 	void setFont(int t, bool inverted);
+	     int write(char data); /* for printf compatibility */
+
+	    int putImg(const unsigned char *img,int x, int y, int opts);
+virtual void plot(int x, int y, int c)=0;
+virtual void clear()=0;
+
+};
 
 #endif
 
--- espardino/gfx/ssd1308.h
+++ espardino/gfx/ssd1308.h
@@ -3,34 +3,34 @@
 
 #include <micro214x.h>
 
-class SSD1308/*: public BitmapLCD*/
+class SSD1308 : public BitmapLCD
 {
 	private:
 		DigitalIO m_DC;
 		DigitalIO m_RST;
 		Spi m_spi;
-		
+
 		unsigned char m_contrast;
 		unsigned char m_scr[128*64/8];
-	
+
 		void set_dc() { m_DC.on();  }
 		void clr_dc() { m_DC.off(); }
 
 		void write_command(unsigned char d);
 		void write_data(unsigned char d);
-		
-			
-		
+
+
+
 	public:
-	
-		
+
+
 		SSD1308();
 		SSD1308(int MISO_pin,int MOSI_pin, int SCK_pin, int CS_pin, int DC_pin, int RST_pin);
 		void attach(int MISO_pin,int MOSI_pin, int SCK_pin, int CS_pin, int DC_pin, int RST_pin);
-		
+
 		void init();
 		void clear();
-		
+
 		void dumpInit();
 		void dump();
 		void setContrast(unsigned char contrast);
